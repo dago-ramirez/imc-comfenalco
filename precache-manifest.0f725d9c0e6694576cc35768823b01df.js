2ce063d9beb0b6f81316223eb4723734578b3862
Dago R <dagoramirezserna@gmail.com>
2020-08-27 19:47:23 -0500

    Updates

---
+++ precache-manifest.0f725d9c0e6694576cc35768823b01df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edd54111accaef34e99bb50638bdb2c5",
+    "url": "/imc-comfenalco/index.html"
+  },
+  {
+    "revision": "cd8ecbe0e9b0ba6f49d4",
+    "url": "/imc-comfenalco/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7d5806a21e7ceb64cfbe",
+    "url": "/imc-comfenalco/static/js/2.a811bf15.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/imc-comfenalco/static/js/2.a811bf15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd8ecbe0e9b0ba6f49d4",
+    "url": "/imc-comfenalco/static/js/main.651f7b6c.chunk.js"
+  },
+  {
+    "revision": "cf5ef7bb299543477c49",
+    "url": "/imc-comfenalco/static/js/runtime-main.e4874b84.js"
+  }
+]);
