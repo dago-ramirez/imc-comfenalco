eb9023e31d2bb2c014277b09053406936252ac3c
Dago R <dagoramirezserna@gmail.com>
2020-08-27 19:37:36 -0500

    Se crea el repositorio

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import CicloDeVida from './components/CicloDeVide'
+
+function App() {
+  return (
+    <div className="App">
+      <CicloDeVida/>
+    </div>
+  );
+}
+
+export default App;
